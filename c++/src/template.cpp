75a00ba719c399bfc42c88ac790aa8582a999754
IdolonInMachina <Cooperbar@gmail.com>
2026-02-16 17:15:48 +1100

    Adds template for each day

---
+++ c++/src/template.cpp
@@ -0,0 +1,16 @@
+#include "../solver.h"
+#include <string>
+#include <vector>
+#include <iostream>
+
+
+
+static std::string solve_dayX_part1(const std::vector<std::string>& lines) {
+    return "";
+}
+
+static std::string solve_dayX_part2(const std::vector<std::string>& lines) {
+    return "";
+}
+
+REGISTER_DAY(0, solve_dayX_part1, solve_dayX_part2);
